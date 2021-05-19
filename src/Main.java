c45bd772ed8dda49440ec00a7e4dadf5abcadb0e
BuildTools <unconfigured@null.spigotmc.org>
2021-05-19 17:01:25 -0400

    Day 2.2

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		Person John = new Person("John", 30);
 		John.hire("Mob Boss");
 		Person Jack = John.adopt("Jack");
-		Time.advanceYear(30);
+		Time.advanceYear(31);
 		Person Cathy = Jack.adopt("Cathy");
 		Time.advanceYear(31);
 		Person Liam = Cathy.adopt("Liam");
